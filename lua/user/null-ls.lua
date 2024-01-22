2db2e9262cb9bbcebdee9848a408bae1d0d600a7
Chandler King <chandleroking@gmail.com>
2024-01-21 21:40:55 -0500

    update formatting

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -19,9 +19,9 @@ function M.config()
       null_ls.builtins.formatting.isort,
       null_ls.builtins.completion.spell,
       null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.black.with({
+      null_ls.builtins.formatting.black.with {
         extra_args = { "--line-length=100" },
-      }),
+      },
     },
   }
 end
