6b97a5fff6fabeba61696f54758400c1f49b779d
Chandler King <chandleroking@gmail.com>
2024-06-19 15:08:44 -0400

    update lualine configuration to be more minimal

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -4,63 +4,58 @@ local M = {
 }
 
 function M.config()
-  local sl_hl = vim.api.nvim_get_hl_by_name("StatusLine", true)
-  vim.api.nvim_set_hl(0, "Copilot", { fg = "#6CC644", bg = sl_hl.background })
-  local icons = require "user.icons"
-  local diff = {
-    "diff",
-    colored = true,
-    symbols = { added = icons.git.LineAdded, modified = icons.git.LineModified, removed = icons.git.LineRemoved }, -- Changes the symbols used by the diff.
-  }
-
-  local copilot = function()
-    local buf_clients = vim.lsp.get_active_clients { bufnr = 0 }
-    if #buf_clients == 0 then
-      return "LSP Inactive"
-    end
-
-    local buf_client_names = {}
-    local copilot_active = false
-
-    for _, client in pairs(buf_clients) do
-      if client.name ~= "null-ls" and client.name ~= "copilot" then
-        table.insert(buf_client_names, client.name)
-      end
-
-      if client.name == "copilot" then
-        copilot_active = true
-      end
-    end
-
-    if copilot_active then
-      return "%#Copilot#" .. icons.git.Octoface .. "%*"
-    end
-    return ""
-  end
-
   require("lualine").setup {
     options = {
-      -- component_separators = { left = "", right = "" },
-      -- section_separators = { left = "", right = "" },
+      icons_enabled = true,
+      theme = "auto",
       component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-
-      ignore_focus = { "NvimTree" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
     },
     sections = {
-      -- lualine_a = { {"branch", icon =""} },
-      -- lualine_b = { diff },
-      -- lualine_c = { "diagnostics" },
-      -- lualine_x = { copilot },
-      -- lualine_y = { "filetype" },
-      -- lualine_z = { "progress" },
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = { diff },
-      lualine_x = { "diagnostics", copilot },
-      lualine_y = { "filetype" },
-      lualine_z = { "progress" },
+      lualine_a = {
+        {
+          "mode",
+          separator = { left = "", right = "" },
+          right_padding = 2,
+        },
+      },
+      lualine_b = {},
+      lualine_c = {
+        { "branch", icon = "", color = { fg = "#fafafa" } },
+        "diff",
+        "diagnostics",
+      },
+      lualine_x = {
+        { "filetype", color = { fg = "#fafafa" } },
+        { "progress", color = { fg = "#fafafa" } },
+        { "location", color = { fg = "#fafafa" } },
+      },
+      lualine_y = {},
+      lualine_z = {},
     },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
     extensions = { "quickfix", "man", "fugitive" },
   }
 end
