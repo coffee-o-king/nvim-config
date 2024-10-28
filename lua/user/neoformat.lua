09a1482122cd2f1184d51e51d0a2420daa57f519
Chandler King <chandleroking@gmail.com>
2024-10-28 18:56:52 -0400

    update black formatter config

--- lua/user/neoformat.lua
+++ lua/user/neoformat.lua
@@ -4,6 +4,7 @@ local M = {
 
 function M.config()
   vim.g.neoformat_run_all_formatters = 1
+  vim.g.neoformat_python_black = { exe = "black", args = { "-l", "100" }, replace = 1 }
   vim.g.neoformat_enabled_python = { "black", "isort", "docformatter" }
 end
 
