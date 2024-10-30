a2d050549774a1cfdf6b97f1eb1b30ed7d47ede0
Chandler King <chandler@hioscar.com>
2024-10-30 09:41:33 -0400

    neoformat settings

--- lua/user/neoformat.lua
+++ lua/user/neoformat.lua
@@ -3,8 +3,11 @@ local M = {
 }
 
 function M.config()
+  -- vim.g.neoformat_verbose = 1
   vim.g.neoformat_run_all_formatters = 1
   vim.g.neoformat_enabled_python = { "black", "isort", "docformatter" }
+  vim.g.neoformat_sql_sleek = { exe = "sleek", args = { "--indent-spaces", 2 }, replace = 1 }
+  vim.g.neoformat_enabled_sql = { "sleek" }
 end
 
 return M
